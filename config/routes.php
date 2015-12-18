f58f9e767ebf5a74826616cc3ed7eff91a932be3
mclantta <mclantta@cs.helsinki.fi>
2015-12-18 20:13:15 +0200

    html pohjia

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,20 @@
 <?php
 
   $routes->get('/', function() {
-    HelloWorldController::index();
+    HelloWorldController::start_page();
   });
 
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
+  });
+  
+  $routes->get('/book/1', function() {
+      HelloWorldController::book_details(); 
+  });
+  
+  $routes->get('/userlist', function() {
+      HelloWorldController::users_own_list();
+  });
+  $routes->get('/edit/1', function() {
+      HelloWorldController::edit();
   });
