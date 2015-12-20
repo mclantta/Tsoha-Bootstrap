8e1bba29f7a708646caedd8d396432765b0c9986
mclantta <mclantta@cs.helsinki.fi>
2015-12-20 14:55:51 +0200

    tietokantatauluja

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,7 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+
+INSERT INTO Reader (name, password) VALUES ('Testi', 'Testinen');
+
+INSERT INTO Book (name, author, publishyear, pages, description) VALUES ('Ylpeys ja ennakkoluulo', 'Jane Austen', '1813', '205', 'Vanha kirja');
+INSERT INTO Book (name, author) VALUES ('Tuulen viemää', 'Margaret Mitchell');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,28 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+
+CREATE TABLE Reader(
+    id SERIAL PRIMARY KEY,
+    name varchar(30) NOT NULL,
+    password varchar(50) NOT NULL
+);
+
+CREATE TABLE List(
+    id SERIAL PRIMARY KEY,
+    reader_id INTEGER REFERENCES Reader(id),
+    name varchar(100) NOT NULL
+);
+
+CREATE TABLE Book(
+    id SERIAL PRIMARY KEY,
+    name varchar(120) NOT NULL,
+    author varchar(80) NOT NULL,
+    publishyear integer,
+    pages integer,
+    description varchar(250)
+);
+
+CREATE TABLE Booklist(
+    list_id INTEGER REFERENCES List(id),
+    book_id INTEGER REFERENCES Book(id)
+);
